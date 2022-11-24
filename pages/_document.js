41e037ad2581e8ac446e7e8cf9fbf06d4227b38a
Israel <almanzaisrael75@gmail.com>
2022-11-24 10:07:30 -0400

    agregando contador de visitas

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+
+
+                <Head>
+
+                    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+
+
+                    <div id="sfchp2n4np4k9wz64tbc2h4847blu3btyy5"></div>
+                    <script type="text/javascript" src="https://counter5.optistats.ovh/private/counter.js?c=hp2n4np4k9wz64tbc2h4847blu3btyy5&down=async" async>
+                    </script>
+                    <noscript>
+                        <a href="https://www.contadorvisitasgratis.com" title="contadores para blogger gratis">
+                        <img src="https://counter5.optistats.ovh/private/contadorvisitasgratis.php?c=hp2n4np4k9wz64tbc2h4847blu3btyy5" border="0" title="contadores para blogger gratis" alt="contadores para blogger gratis"/>
+                    </a></noscript>
+                </Head>
+
+                <body>
+
+                    <Main />
+                    <NextScript />
+
+
+
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
